9f77489b33743deb60b31f3a71aa0e6767b84eee
eneserciyes <m.eneserciyes@gmail.com>
2024-02-03 16:07:23 -0500

    added downsample, upsample

--- examples/rl/iris/nets.py
+++ examples/rl/iris/nets.py
@@ -1,7 +1,40 @@
 from tinygrad import Tensor, nn
 from typing import Optional
+from itertools import chain
 
 def swish(x: Tensor): return x.mul(x.sigmoid())
+
+# taken from "https://github.com/tinygrad/tinygrad/blob/master/examples/yolov8.py" (now 3 models use nearest upsampling)
+def upsample(x: Tensor, scale_factor: float) -> Tensor:
+  assert len(x.shape) > 2 and len(x.shape) <= 5
+  (b, c), _lens = x.shape[:2], len(x.shape[2:])
+  tmp = x.reshape([b, c, -1] + [1] * _lens) * Tensor.ones(*[1, 1, 1] + [scale_factor] * _lens)
+  return tmp.reshape(list(x.shape) + [scale_factor] * _lens).permute([0, 1] + list(chain.from_iterable([[y+2, y+2+_lens] for y in range(_lens)]))).reshape([b, c] + [x * scale_factor for x in x.shape[2:]])
+
+class Upsample:
+  def __init__(self, in_channels:int, with_conv:bool) -> None:
+    self.with_conv = with_conv
+    if self.with_conv:
+      self.conv = nn.Conv2d(in_channels, in_channels, 3, 1, 1)
+
+  def __call__(self, x: Tensor) -> Tensor:
+    if self.with_conv:
+      return self.conv(upsample(x, scale_factor=2))
+
+    return upsample(x, scale_factor=2)
+
+class Downsample:
+  def __init__(self, in_channels:int, with_conv:bool) -> None:
+    self.with_conv = with_conv
+    if self.with_conv:
+      self.conv = nn.Conv2d(in_channels, in_channels, 3, 2, 0)
+
+  def __call__(self, x: Tensor) -> Tensor:
+    if self.with_conv:
+      pad = (0, 1, 0, 1)
+      return self.conv(x.pad2d(pad, 0))
+
+    return x.avg_pool2d(kernel_size=(2, 2), stride=2)
 
 class ResnetBlock:
   def __init__(self, in_channels:int, out_channels:Optional[int] = None, conv_shortcut:bool=False,
@@ -77,4 +110,3 @@ class ResAttnBlock:
 
   def __call__(self, x: Tensor) -> Tensor:
     return x + self.attn(self.norm(x))
-
