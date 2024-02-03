0baf4dccd7537ac9ad9a7c1dbb0ec8fa917d9a5d
eneserciyes <m.eneserciyes@gmail.com>
2024-02-03 17:21:06 -0500

    test files for res blocks

---
+++ examples/rl/iris/test_resblocks.py
@@ -0,0 +1,18 @@
+import unittest
+import numpy as np
+
+from nets import ResnetBlock
+from tinygrad import Tensor
+
+
+class TestResnetBlock(unittest.TestCase):
+  def test_forward(self):
+    res_block = ResnetBlock(in_channels=32, out_channels=32, temb_channels=0, dropout=0.)
+    x = Tensor(np.load("test_files/resblock_in.npy"))
+    y = res_block(x, None)
+    y_expected = np.load("test_files/resblock_out_same_channels.npy")
+    np.testing.assert_allclose(y.numpy(), y_expected, atol=1e-5)
+
+
+if __name__ == '__main__':
+  unittest.main()
